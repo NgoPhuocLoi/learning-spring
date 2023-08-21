568ea071f86270b077f1ee34ea852ef9f44fe4f0
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-08-21 22:52:49 +0700

    Getting form data using @RequestParam annotation (In SpringMVC, 'request parameters map to query params, form data and parts in multiparts request')

--- easy_school_app/src/main/java/com/example/easy_school_app/controllers/ContactController.java
+++ easy_school_app/src/main/java/com/example/easy_school_app/controllers/ContactController.java
@@ -1,13 +1,28 @@
 package com.example.easy_school_app.controllers;
 
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContactController {
 
-    @RequestMapping("/contact")
+    @RequestMapping(value = "/contact")
     public String displayContactPage() {
         return "contact.html";
+    }
+
+    // @RequestMapping(value = "/saveMsg", method = POST)
+    @PostMapping(value = "/saveMsg")
+    public ModelAndView handleSaveMessage(@RequestParam(required = true, name = "name") String name,
+            @RequestParam String mobileNum) {
+        System.out.println("Hello");
+        System.out.println("name: " + name);
+        System.out.println("mobileNum: " + mobileNum);
+        return new ModelAndView("redirect:/contact");
     }
 }
