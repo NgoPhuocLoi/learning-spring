728d299109b89fc121ca626c2c4bd0da5d850bd7
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-08-24 23:25:04 +0700

    Handle Beans validation

--- easy_school_app/src/main/java/com/example/easy_school_app/controllers/ContactController.java
+++ easy_school_app/src/main/java/com/example/easy_school_app/controllers/ContactController.java
@@ -2,12 +2,19 @@ package com.example.easy_school_app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.easy_school_app.models.Contact;
 import com.example.easy_school_app.services.ContactService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ContactController {
@@ -19,26 +26,36 @@ public class ContactController {
         this.contactService = contactService;
     }
 
-    @RequestMapping(value = "/contact")
-    public String displayContactPage() {
+    @RequestMapping("/contact")
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
-    // @RequestMapping(value = "/saveMsg", method = POST)
-    // @PostMapping(value = "/saveMsg")
-    // public ModelAndView handleSaveMessage(@RequestParam(required = true, name =
-    // "name") String name,
-    // @RequestParam String mobileNum) {
-    // System.out.println("Hello");
-    // System.out.println("name: " + name);
-    // System.out.println("mobileNum: " + mobileNum);
-    // return new ModelAndView("redirect:/contact");
-    // }
+    /*
+     * @RequestMapping(value = "/saveMsg",method = POST)
+     * public ModelAndView saveMessage(@RequestParam String name, @RequestParam
+     * String mobileNum,
+     * 
+     * @RequestParam String email, @RequestParam String subject, @RequestParam
+     * String message) {
+     * log.info("Name : " + name);
+     * log.info("Mobile Number : " + mobileNum);
+     * log.info("Email Address : " + email);
+     * log.info("Subject : " + subject);
+     * log.info("Message : " + message);
+     * return new ModelAndView("redirect:/contact");
+     * }
+     */
 
-    // using POJO class to handle Request Params
-    @PostMapping(value = "/saveMsg")
-    public ModelAndView handleSaveMessage(Contact contact) {
+    @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+
+        if (errors.hasErrors()) {
+            // System.out.println(errors);
+            return "contact.html";
+        }
         contactService.saveMessageToDB(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
--- easy_school_app/src/main/java/com/example/easy_school_app/models/Contact.java
+++ easy_school_app/src/main/java/com/example/easy_school_app/models/Contact.java
@@ -1,9 +1,19 @@
 package com.example.easy_school_app.models;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class Contact {
-    private String name, mobileNum, email, subject, message;
+    @NotBlank(message = "Name must not be blank")
+    @Min(3)
+    private String name;
+
+    @NotBlank(message = "ABC")
+    private String mobileNum;
+    private String email;
+    private String subject;
+    private String message;
 
 }
